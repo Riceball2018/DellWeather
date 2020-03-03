2408a2c8337852e0c5a9995c65686627bed63762
Mark Chen <markchenzj@gmail.com>
2020-03-04 01:45:19 +0800

    Add weather query service

--- DellWeatherApp/Webservice/WeatherService.cs
+++ DellWeatherApp/Webservice/WeatherService.cs
@@ -5,16 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using DellWeatherApp.JsonParsing;
+using Newtonsoft.Json;
 
 namespace DellWeatherApp.Webservice
 {
     public class WeatherService
     {
-        private readonly string apiCall = "api.openweathermap.org/data/2.5/weather";
+        private static readonly string apiCall = "api.openweathermap.org/data/2.5/weather";
+        private static readonly string apiKey  = "&appid=4f094915199158e6e7d09c671410ba08";
 
-        public static async WeatherModel GetWeather(int cityId)
+        private static HttpClient client = new HttpClient();
+
+        // TODO: Add cancellation token
+        public static async Task<WeatherModel> GetWeather(int cityId)
         {
-            HttpClient
+            string weatherRequest = apiCall + "?" + cityId + apiKey;
+            Console.WriteLine(weatherRequest);
+
+            string response = await client.GetStringAsync(weatherRequest);
+
+            WeatherModel weatherModel = JsonConvert.DeserializeObject<WeatherModel>(response, new JsonConverter[] { new WeatherJsonConverter() } );
+
+            return weatherModel;
         }
     }
 }
