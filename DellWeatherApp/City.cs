88c1b03382b522597507d3614bcc185500487bbb
Mark Chen <markchenzj@gmail.com>
2020-03-03 21:56:49 +0800

    Initial commit. Add basic weather class. Add basic city class

---
+++ DellWeatherApp/City.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DellWeatherApp
+{
+    public class City
+    {
+        public Weather CityWeather { get; set; }         // Weather information for the city
+        public string Name { get; private set; }         // Name of the city 
+        public DateTime CacheTime { get; private set; }  // Time weather information was retrieved
+
+        private List<Weather> weatherCache;
+
+        public City(string cityName)
+        {
+            Name = cityName;
+
+            CityWeather = new Weather();
+        }
+    }
+
+    public class CityViewModel
+    {
+        private ObservableCollection<City> cities = new ObservableCollection<City>();
+        public ObservableCollection<City> Cities { get { return cities; } }
+
+        public CityViewModel()
+        {
+            cities.Add(new City("Singapore"));
+            cities.Add(new City("Taipei"));
+            cities.Add(new City("Austin"));
+            cities.Add(new City("San Francisco"));
+        }
+    }
+}
---
+++ DellWeatherApp/Weather.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DellWeatherApp
+{
+    public class Weather
+    {
+        public float LowTemp { get; set; }
+        public float HighTemp { get; set; }
+        public float CurrTemp { get; set; }
+        public float Humidity { get; set; }
+        public DateTime Sunrise { get; set; }
+        public DateTime Sunset { get; set; }
+
+        public Weather(float lowTemp, float highTemp, float currTemp, float humidity, DateTime sunrise, DateTime sunset)
+        {
+            LowTemp  = lowTemp;
+            HighTemp = highTemp;
+            CurrTemp = currTemp;
+            Humidity = humidity;
+            Sunrise  = sunrise;
+            Sunset   = sunset;
+        }
+
+        public Weather() : this(0, 0, 0, 0, DateTime.MinValue, DateTime.MinValue) { }
+    }
+}
